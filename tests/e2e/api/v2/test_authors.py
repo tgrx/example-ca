29c84c1d63ea69d8ababdbfb7e4014c7f5506f34
Alexander Sidorov <3865736+tgrx@users.noreply.github.com>
2023-11-17 00:52:17 +0300

    refactored authors test for deletion

--- tests/e2e/api/v2/test_authors.py
+++ tests/e2e/api/v2/test_authors.py
@@ -2,21 +2,48 @@ import os
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from app.entities.models import Author
     from clientlib.client import AppClient
 
 
-def test_user_can_create_and_retrieve_author(
+def test_author_crud(
     *,
     client: "AppClient",
 ) -> None:
     salt = os.urandom(4).hex()
-    name = f"author {salt}"
+    name1 = f"author-{salt}-1"
 
-    author_created = client.create_author(name=name)
+    assert_no_author(client, name1)
 
-    assert author_created.id
-    assert author_created.name == name
+    author_created = assert_author_created(client, name1)
+    assert_author_exists(client, author_created)
 
-    author_retrieved = client.get_author_by_id(id=author_created.id)
+    delete_author(client, author_created)
+    assert_no_author(client, name1)
 
-    assert author_retrieved == author_created
+
+def assert_no_author(client: "AppClient", name: str) -> None:
+    authors = client.get_all_authors()
+    authors_with_name = {
+        author.id for author in authors if author.name == name
+    }
+    assert len(authors_with_name) == 0
+
+
+def assert_author_created(client: "AppClient", name: str) -> "Author":
+    author = client.create_author(name=name)
+    assert author.id
+    assert author.name == name
+    return author
+
+
+def assert_author_exists(client: "AppClient", author: "Author") -> None:
+    authors = client.get_all_authors()
+    assert author in authors
+
+    author_retrieved = client.get_author_by_id(id=author.id)
+    assert author_retrieved == author
+
+
+def delete_author(client: "AppClient", author: "Author") -> None:
+    client.delete_author_by_id(id=author.id)
