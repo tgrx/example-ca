80b01319aa7d46bc6616ddf12da7a85adf44b0d8
Alexander Sidorov <3865736+tgrx@users.noreply.github.com>
2023-11-17 23:26:34 +0300

    client code refactored (#8)

--- clientlib/client.py
+++ clientlib/client.py
@@ -1,9 +1,18 @@
+from contextlib import suppress
 from typing import TYPE_CHECKING
+from typing import Any
+from typing import Collection
+from typing import Type
+from typing import TypeVar
+from typing import cast
 
 import attrs
+import orjson
 import tenacity
+from pydantic import BaseModel
 
 from clientlib.entities import AllAuthorsResponse
+from clientlib.entities import ApiResponse
 from clientlib.entities import CreateAuthorRequest
 from clientlib.entities import CreateAuthorResponse
 from clientlib.entities import DeleteAuthorResponse
@@ -16,8 +25,12 @@ if TYPE_CHECKING:
     from uuid import UUID
 
     from httpx import Client
+    from httpx import Response
 
     from app.entities.models import Author
+
+
+T = TypeVar("T")
 
 
 retry = tenacity.retry(
@@ -39,182 +52,152 @@ class AppClient:
 
     session: "Client"
 
-    @retry
     def create_author(self, *, name: str) -> "Author":
-        request = CreateAuthorRequest(name=name)
-        request_body = request.model_dump_json()
-
-        response = self.session.post(
-            "/api/v2/authors/",
-            content=request_body,
+        author = self._api_call(
+            method="post",
+            path="/api/v2/authors/",
+            request=CreateAuthorRequest(name=name),
+            response_cls=CreateAuthorResponse,
+            statuses=(201,),
         )
-
-        if response.status_code != 201:
-            raise AppClientError(
-                "unsuccessful api call",
-                code=response.status_code,
-                headers=dict(response.headers.items()),
-                payload=response.text,
-            )
-
-        payload = CreateAuthorResponse.model_validate_json(response.text)
-        if payload.errors:
-            raise AppClientError(
-                "cannot create author",
-                code=response.status_code,
-                headers=dict(response.headers.items()),
-                payload=response.text,
-            )
-
-        author = payload.data
 
         return author
 
-    @retry
     def delete_author_by_id(
         self,
         *,
         id: "UUID",  # noqa: A002
     ) -> None:
-        response = self.session.delete(f"/api/v2/authors/{id}/")
+        return self._api_call(
+            method="delete",
+            path=f"/api/v2/authors/{id}/",
+            response_cls=DeleteAuthorResponse,
+        )
 
-        if response.status_code != 200:
-            raise AppClientError(
-                "unsuccessful api call",
-                code=response.status_code,
-                headers=dict(response.headers.items()),
-                payload=response.text,
-            )
-
-        payload = DeleteAuthorResponse.model_validate_json(response.text)
-        if payload.errors:
-            raise AppClientError(
-                "cannot delete author",
-                code=response.status_code,
-                headers=dict(response.headers.items()),
-                payload=response.text,
-            )
-
-        return payload.data
-
-    @retry
     def get_all_authors(self) -> list["Author"]:
-        response = self.session.get("/api/v1/authors/")
-        if response.status_code != 200:
-            raise AppClientError(
-                "unsuccessful api call",
-                code=response.status_code,
-                headers=dict(response.headers.items()),
-                payload=response.text,
-            )
-
-        payload = AllAuthorsResponse.model_validate_json(response.text)
-        if payload.errors:
-            raise AppClientError(
-                "cannot get authors",
-                code=response.status_code,
-                headers=dict(response.headers.items()),
-                payload=response.text,
-            )
-
-        authors = payload.data
+        authors = self._api_call(
+            method="get",
+            path="/api/v1/authors/",
+            response_cls=AllAuthorsResponse,
+        )
 
         return authors
 
-    @retry
     def get_author_by_id(
         self,
         id: "UUID",  # noqa: A002,VNE003
     ) -> "Author":
-        response = self.session.get(f"/api/v2/authors/{id}/")
-
-        if response.status_code != 200:
-            raise AppClientError(
-                "unsuccessful api call",
-                code=response.status_code,
-                headers=dict(response.headers.items()),
-                payload=response.text,
-            )
-
-        payload = GetAuthorResponse.model_validate_json(response.text)
-        if payload.errors:
-            raise AppClientError(
-                "cannot get author",
-                code=response.status_code,
-                headers=dict(response.headers.items()),
-                payload=response.text,
-            )
-
-        author = payload.data
+        author = self._api_call(
+            method="get",
+            path=f"/api/v2/authors/{id}/",
+            response_cls=GetAuthorResponse,
+            statuses=(200, 404),
+        )
 
         return author
 
-    @retry
     def get_author_by_name(
         self,
         name: str,  # noqa: A002,VNE003
     ) -> "Author":
-        response = self.session.get(
-            "/api/v2/authors/",
-            params={
-                "name": name,
-            },
+        authors = self._api_call(
+            method="get",
+            params={"name": name},
+            path="/api/v2/authors/",
+            response_cls=AllAuthorsResponse,
         )
 
-        if response.status_code != 200:
-            raise AppClientError(
-                "unsuccessful api call",
-                code=response.status_code,
-                headers=dict(response.headers.items()),
-                payload=response.text,
-            )
+        if not authors:
+            raise AppClientError(f"no author with {name=!r}")
 
-        payload = AllAuthorsResponse.model_validate_json(response.text)
-        if payload.errors:
-            raise AppClientError(
-                "cannot get author",
-                code=response.status_code,
-                headers=dict(response.headers.items()),
-                payload=response.text,
-            )
-
-        authors = payload.data
         author = authors[0]
 
         return author
 
-    @retry
     def update_author(
         self,
         *,
         id: "UUID",  # noqa: A002
         name: str,
     ) -> "Author":
-        request = UpdateAuthorRequest(name=name)
-        request_body = request.model_dump_json()
-
-        response = self.session.patch(
-            f"/api/v2/authors/{id}/",
-            content=request_body,
+        author = self._api_call(
+            method="patch",
+            path=f"/api/v2/authors/{id}/",
+            request=UpdateAuthorRequest(name=name),
+            response_cls=UpdateAuthorResponse,
+            statuses=(200,),
         )
 
-        if response.status_code != 200:
-            raise AppClientError(
-                "unsuccessful api call",
-                code=response.status_code,
-                headers=dict(response.headers.items()),
-                payload=response.text,
+        return author
+
+    def _api_call(
+        self,
+        *,
+        method: str,
+        params: dict | None = None,
+        path: str,
+        request: BaseModel | None = None,
+        response_cls: Type[ApiResponse[T]],
+        statuses: Collection[int] = (200,),
+    ) -> T:
+        content = None
+        if request:
+            content = request.model_dump_json(by_alias=True)
+
+        def pretty(body: str | bytes | None) -> Any:
+            if body is None:
+                return body
+
+            with suppress(orjson.JSONDecodeError):
+                return orjson.loads(body)
+
+            if isinstance(body, bytes):
+                body = body.decode()
+
+            return body
+
+        @retry
+        def _request() -> "Response":
+            return self.session.request(
+                content=content,
+                method=method,
+                params=params,
+                url=path,
             )
 
-        payload = UpdateAuthorResponse.model_validate_json(response.text)
+        try:
+            rs = _request()
+        except tenacity.RetryError as err:
+            raise AppClientError(
+                f"http request failed: {err}",
+                request_body=pretty(content),
+                request_headers=dict(self.session.headers.items()),
+                request_method=method,
+                request_params=params,
+                request_path=path,
+            ) from err
+
+        if rs.status_code not in statuses:
+            raise AppClientError(
+                "api call failed",
+                request_body=pretty(rs.request.content),
+                request_headers=dict(rs.request.headers.items()),
+                request_method=rs.request.method,
+                request_params=params,
+                request_path=str(rs.request.url),
+                response_body=pretty(rs.text),
+                response_code=rs.status_code,
+                response_headers=dict(rs.headers.items()),
+            )
+
+        payload = response_cls.model_validate_json(rs.text)
         if payload.errors:
             raise AppClientError(
-                "cannot update author",
-                code=response.status_code,
-                headers=dict(response.headers.items()),
-                payload=response.text,
+                "\n".join(payload.errors),
+                request_method=rs.request.method,
+                request_params=params,
+                request_path=str(rs.request.url),
+                response_code=rs.status_code,
             )
 
-        author = payload.data
-
-        return author
+        return cast(T, payload.data)
