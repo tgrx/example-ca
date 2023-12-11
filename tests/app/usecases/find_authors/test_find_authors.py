ad3b4bc40ee282434c525ad0ac430e1973a7a37b
Alexander Sidorov <3865736+tgrx@users.noreply.github.com>
2023-12-11 22:00:30 +0300

    fixed unit tests

--- tests/app/usecases/find_authors/test_find_authors.py
+++ tests/app/usecases/find_authors/test_find_authors.py
@@ -1,7 +1,10 @@
+import pytest
+
 from app.entities.models import Author
 from app.usecases.author import FindAuthorsUseCase
 
 
+@pytest.mark.unit
 def test_find_all(
     find_authors: FindAuthorsUseCase,
     grimm_jacob: Author,
@@ -11,22 +14,24 @@ def test_find_all(
     assert authors == [grimm_jacob, grimm_wilhelm]
 
 
+@pytest.mark.unit
 def test_find_by_name(
     find_authors: FindAuthorsUseCase,
     grimm_jacob: Author,
     grimm_wilhelm: Author,
 ) -> None:
-    for author in {grimm_jacob, grimm_wilhelm}:
+    for author in [grimm_jacob, grimm_wilhelm]:
         found = find_authors(name=author.name)
         assert found == [author]
 
 
+@pytest.mark.unit
 def test_find_by_pk(
     find_authors: FindAuthorsUseCase,
     grimm_jacob: Author,
     grimm_wilhelm: Author,
 ) -> None:
-    for author in {grimm_jacob, grimm_wilhelm}:
+    for author in [grimm_jacob, grimm_wilhelm]:
         found = find_authors(author_id=author.author_id)
         assert found == [author]
 
--- tests/app/usecases/find_books/test_find_books.py
+++ tests/app/usecases/find_books/test_find_books.py
@@ -1,7 +1,10 @@
+import pytest
+
 from app.entities.models import Book
 from app.usecases.book import FindBooksUseCase
 
 
+@pytest.mark.unit
 def test_find_all(
     find_books: FindBooksUseCase,
     finnegans_wake: Book,
@@ -11,22 +14,24 @@ def test_find_all(
     assert books == [finnegans_wake, ulysses]
 
 
+@pytest.mark.unit
 def test_find_by_pk(
     find_books: FindBooksUseCase,
     finnegans_wake: Book,
     ulysses: Book,
 ) -> None:
-    for book in {finnegans_wake, ulysses}:
+    for book in [finnegans_wake, ulysses]:
         found = find_books(book_id=book.book_id)
         assert found == [book]
 
 
+@pytest.mark.unit
 def test_find_by_title(
     find_books: FindBooksUseCase,
     finnegans_wake: Book,
     ulysses: Book,
 ) -> None:
-    for book in {finnegans_wake, ulysses}:
+    for book in [finnegans_wake, ulysses]:
         found = find_books(title=book.title)
         assert found == [book]
 
